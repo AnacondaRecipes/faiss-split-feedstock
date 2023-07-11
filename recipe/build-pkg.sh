5ece05cf64418f6f56263c256a2317df493914be
Anton Kabanovs <akabanovs@anaconda.com>
2023-07-11 15:43:40 +0300

    Cleaning up pt 3

--- recipe/build-pkg.sh
+++ recipe/build-pkg.sh
@@ -11,32 +11,59 @@ else
 fi
 
 # Build vanilla version (no avx2), see build-lib.sh
-cmake -G Ninja \
-    ${CMAKE_ARGS} \
-    -Dfaiss_ROOT=_libfaiss_generic_stage/ \
-    -DFAISS_ENABLE_GPU=${FAISS_ENABLE_GPU} \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DCMAKE_LIBRARY_PATH=${PREFIX}/lib \
-    -DPython_EXECUTABLE="${PYTHON}" \
-    -B _build_python_generic \
-    faiss/python
-cmake --build _build_python_generic --target swigfaiss -j $CPU_COUNT
+if [[ "${target_platform}" == "osx-64" ]]; then
+  cmake -G Ninja \
+      ${CMAKE_ARGS} \
+      -Dfaiss_ROOT=_libfaiss_generic_stage/ \
+      -DFAISS_ENABLE_GPU=${FAISS_ENABLE_GPU} \
+      -DOpenMP_CXX_FLAGS=-fopenmp=libiomp5 \
+      -DOpenMP_CXX_LIB_NAMES=libiomp5 \
+      -DOpenMP_libiomp5_LIBRARY=$PREFIX/lib/libiomp5.dylib \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DPython_EXECUTABLE="${PYTHON}" \
+      -B _build_python_generic \
+      faiss/python
+  cmake --build _build_python_generic --target swigfaiss -j $CPU_COUNT
+else
+  cmake -G Ninja \
+      ${CMAKE_ARGS} \
+      -Dfaiss_ROOT=_libfaiss_generic_stage/ \
+      -DFAISS_ENABLE_GPU=${FAISS_ENABLE_GPU} \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DPython_EXECUTABLE="${PYTHON}" \
+      -B _build_python_generic \
+      faiss/python
+  cmake --build _build_python_generic --target swigfaiss -j $CPU_COUNT
+fi
 
 # Build version with avx2 support, see build-lib.sh
 if [[ "${target_platform}" == *-64 ]]; then
-    cmake -G Ninja \
-        ${CMAKE_ARGS} \
-        -Dfaiss_ROOT=_libfaiss_avx2_stage/ \
-        -DFAISS_OPT_LEVEL=avx2 \
-        -DFAISS_ENABLE_GPU=${FAISS_ENABLE_GPU} \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_INSTALL_PREFIX=$PREFIX \
-        -DCMAKE_LIBRARY_PATH=${PREFIX}/lib \
-        -DPython_EXECUTABLE="${PYTHON}" \
-        -B _build_python_avx2 \
-        faiss/python
-    cmake --build _build_python_avx2 --target swigfaiss_avx2 -j $CPU_COUNT
+    if [[ "${target_platform}" == "osx-64" ]]; then
+        cmake -G Ninja \
+            ${CMAKE_ARGS} \
+            -Dfaiss_ROOT=_libfaiss_avx2_stage/ \
+            -DOpenMP_CXX_FLAGS=-fopenmp=libiomp5 \
+            -DOpenMP_CXX_LIB_NAMES=libiomp5 \
+            -DOpenMP_libiomp5_LIBRARY=$PREFIX/lib/libiomp5.dylib \
+            -DFAISS_OPT_LEVEL=avx2 \
+            -DFAISS_ENABLE_GPU=${FAISS_ENABLE_GPU} \
+            -DCMAKE_BUILD_TYPE=Release \
+            -DPython_EXECUTABLE="${PYTHON}" \
+            -B _build_python_avx2 \
+            faiss/python
+        cmake --build _build_python_avx2 --target swigfaiss_avx2 -j $CPU_COUNT
+    else
+        cmake -G Ninja \
+            ${CMAKE_ARGS} \
+            -Dfaiss_ROOT=_libfaiss_avx2_stage/ \
+            -DFAISS_OPT_LEVEL=avx2 \
+            -DFAISS_ENABLE_GPU=${FAISS_ENABLE_GPU} \
+            -DCMAKE_BUILD_TYPE=Release \
+            -DPython_EXECUTABLE="${PYTHON}" \
+            -B _build_python_avx2 \
+            faiss/python
+        cmake --build _build_python_avx2 --target swigfaiss_avx2 -j $CPU_COUNT
+    fi
 
     # copy generated swig module with avx2-support to specifically named file, cf.
     # https://github.com/facebookresearch/faiss/blob/v1.7.1/faiss/python/setup.py#L37-L40
